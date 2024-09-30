5b61090b0cc3133fe85ffebc6770ca8a65828cbd
Cleo <cleo-aron.weis@gmx.de>
2024-09-30 13:39:43 +0200

    vips support added

--- dinov2/data/datasets/CustomImageDataset.py
+++ dinov2/data/datasets/CustomImageDataset.py
@@ -8,8 +8,10 @@ from torchvision import transforms
 #from sklearn.model_selection import train_test_split
 from .decoders import ImageDataDecoder
 import random
+import pyvips
+import numpy as np
 #import numpy as np
-#from PIL import Image
+from PIL import Image
 
 # these functions were just created to check how the augmented images/crops look like
 def save_image(tensor, name1, name2):
@@ -41,6 +43,17 @@ def save_all(image_pil, name1):
     for i, image in enumerate(local_crops):
         save_image(image, name1, 'local_crop'+str(i))
 
+def check_file_path(file_list_in, pattern = ['png', 'vips']):
+
+    file_list_out = []
+    for i_file in file_list_in:
+        for i_pattern in pattern:
+            if i_file.endswith('.' + i_pattern):
+                if not "/._" in i_file:
+                    file_list_out.append(i_file)
+
+    return file_list_out
+
 
 class CustomImageDataset(Dataset):
     def __init__(
@@ -55,23 +68,38 @@ class CustomImageDataset(Dataset):
         self.transform = transform
         self.target_transform = target_transform
         self.split = split
-
+        self.file_list = [f for f in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, f)) and not f.startswith('.')]
+        len(self.file_list)
+        self.file_list = check_file_path(self.file_list)
+        len(self.file_list)
+        #print(self.file_list)
         # split into train and test
         #self.train_data, self.test_data = train_test_split(
         #    self.img_labels, test_size=test_size, random_state=random_state)
 
     def __len__(self):
         #return len(self.img_labels) 
-        return len([f for f in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, f))])
+        return len(self.file_list) #len([f for f in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, f))])
 
     def __getitem__(self, idx):
         try:
-            img_path = [f for f in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, f))][idx]
+            img_path = self.file_list[idx]
             img_path = os.path.join(self.img_dir, img_path)
-            with open(img_path, mode="rb") as f:
-                image_pil = f.read()
-            image_pil = ImageDataDecoder(image_pil).decode()
+            #print("does work until here")
+            if 'vips' not in img_path:
+                with open(img_path, mode="rb") as f:
+                    image_pil = f.read()
+                image_pil = ImageDataDecoder(image_pil).decode()
+            else:
+                #print(img_path)
+
+                vips_image = pyvips.Image.new_from_file(img_path)
+                numpy_image = np.array(vips_image)
+                image_pil = Image.fromarray(numpy_image)#.tobytes()
+                print("does work until here")
+
         except Exception as e:
+            print(f"error with {img_path}")
             # in case of error when reading image, just take a random different one
             random_index = random.randint(0, len(self) - 1)
             image = self.__getitem__(random_index)
